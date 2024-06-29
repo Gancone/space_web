a7b963432f6374febe256a399d5bcc68793db8e1
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2024-06-29 21:31:50 +0200

    Inizializzato il progetto, incorporato bootstrap, creato il componente Main (solo la vista), con al ssuo interno vite

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite(['resources\css\app.css','resources\js\app.js'])
+        <title>Bootstrap demo</title>
+    </head>
+
+    <body>
+        <main>
+            {{$slot}}
+        </main>
+    </body>
+
+    </html>
+</div>
